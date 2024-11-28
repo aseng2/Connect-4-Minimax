fe7af1bca57bcebbb3b89877de3dae0ed4c32293
justindnguyen <justindnguyen03@gmail.com>
2024-11-27 22:44:23 -0800

    minimax algorithm implemented

--- connect4.py
+++ connect4.py
@@ -82,6 +82,101 @@ def get_valid_locations(board):
             valid_locations.append(c)
     return valid_locations
 
+def is_terminal_node(board):
+    return winning_move(board, PLAYER1_PIECE) or winning_move(board, PLAYER2_PIECE) or len(get_valid_locations(board)) == 0
+
+def score_position(board, piece):
+    score = 0
+    center_array = [int(board[r][COLUMN_COUNT//2]) for r in range(ROW_COUNT)]
+    center_count = center_array.count(piece)
+    score += center_count * 3
+
+    for r in range(ROW_COUNT):
+        row_array = [int(i) for i in list(board[r,:])]
+        for c in range(COLUMN_COUNT-3):
+            window = row_array[c:c+4]
+            score += evaluate_window(window, piece)
+
+    for c in range(COLUMN_COUNT):
+        col_array = [int(board[r][c]) for r in range(ROW_COUNT)]
+        for r in range(ROW_COUNT-3):
+            window = col_array[r:r+4]
+            score += evaluate_window(window, piece)
+
+    for r in range(ROW_COUNT-3):
+        for c in range(COLUMN_COUNT-3):
+            window = [board[r+i][c+i] for i in range(4)]
+            score += evaluate_window(window, piece)
+
+    for r in range(ROW_COUNT-3):
+        for c in range(COLUMN_COUNT-3):
+            window = [board[r+3-i][c+i] for i in range(4)]
+            score += evaluate_window(window, piece)
+
+    return score
+
+def evaluate_window(window, piece):
+    score = 0
+    opponent_piece = PLAYER1_PIECE if piece == PLAYER2_PIECE else PLAYER2_PIECE
+
+    if window.count(piece) == 4:
+        score += 100
+    elif window.count(piece) == 3 and window.count(0) == 1:
+        score += 5
+    elif window.count(piece) == 2 and window.count(0) == 2:
+        score += 2
+
+    if window.count(opponent_piece) == 3 and window.count(0) == 1:
+        score -= 4
+
+    return score
+
+def minimax(board, depth, alpha, beta, maximizingPlayer):
+    valid_locations = get_valid_locations(board)
+    is_terminal = is_terminal_node(board)
+
+    if depth == 0 or is_terminal:
+        if is_terminal:
+            if winning_move(board, PLAYER2_PIECE):
+                return (None, 100000000000000)
+            elif winning_move(board, PLAYER1_PIECE):
+                return (None, -10000000000000)
+            else: # No more valid moves
+                return (None, 0)
+        else: # Depth is zero
+            return (None, score_position(board, PLAYER2_PIECE))
+    if maximizingPlayer:
+        value = -math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, PLAYER2_PIECE)
+            new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
+            if new_score > value:
+                value = new_score
+                column = col
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return column, value
+
+    else: # Minimizing player
+        value = math.inf
+        column = random.choice(valid_locations)
+        for col in valid_locations:
+            row = get_open_row(board, col)
+            b_copy = board.copy()
+            drop_piece(b_copy, row, col, PLAYER1_PIECE)
+            new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
+            if new_score < value:
+                value = new_score
+                column = col
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return column, value
+
 # Create empty board
 board = create_board()
 
@@ -111,17 +206,26 @@ while not game_over:
         turn +=1
 
     #This will turn into the AI 
-    if turn % 2 == PLAYER2_TURN and not game_over:
-        col = int(input("Player 2 Make your selection (0-6): "))
+    # if turn % 2 == PLAYER2_TURN and not game_over:
+    #     col = int(input("Player 2 Make your selection (0-6): "))
+    #     if is_valid_location(board, col):
+    #         row = get_open_row(board, col)
+    #         drop_piece(board, row, col, PLAYER2_PIECE)
+
+    #         if winning_move(board, PLAYER2_PIECE):
+    #             print("Player 2 win, congratulations!")
+    #             game_over = True 
+    #             print_board(board)
+    #             break
+    else:
+        col, minimax_score = minimax(board, 5, -math.inf, math.inf, True)
         if is_valid_location(board, col):
             row = get_open_row(board, col)
             drop_piece(board, row, col, PLAYER2_PIECE)
 
             if winning_move(board, PLAYER2_PIECE):
-                print("Player 2 win, congratulations!")
-                game_over = True 
-                print_board(board)
-                break
+                print("AI wins!")
+                game_over = True
         
         print_board(board)
         print("\n")
